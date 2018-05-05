fabf73ced7b276d3fe32b0ca8621040076ab0fc4
Lab103 <Lab103@LAB101-19>
2018-05-04 23:26:57 -0300

    código atualizado

--- ControleDeEmprestimo/teste/br/emprestimo/dadosDeTeste/ObtemLivro.java
+++ ControleDeEmprestimo/teste/br/emprestimo/dadosDeTeste/ObtemLivro.java
@@ -1,5 +1,26 @@
 package br.emprestimo.dadosDeTeste;
 
-public class ObtemLivro {
+import br.emprestimo.modelo.Livro;
 
+public class ObtemLivro {
+	public static Livro comDadosValidos(){
+		
+		Livro livro = new Livro();
+		livro.setIsbn("121212");
+		livro.setTitulo("Engenharia de software");
+		livro.setAutor("Presmann");
+		return livro;		
+	}
+	
+	public static Livro comISBNInvalido_branco(){
+		Livro livro = new Livro();
+		livro.setIsbn("");
+		return livro;
+	}
+
+	public static Livro comISBNInvalido_nulo(){
+		Livro livro = new Livro();
+		livro.setIsbn(null);
+		return livro;
+	}
 }
